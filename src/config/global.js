2f458eeaec1a5dbb2ab2d62dded1ed024a06d200
Swathysuriyababu12 <swathysuryababu@gmail.com>
2023-09-16 11:40:07 +0530

    deployed links updated

--- src/config/global.js
+++ src/config/global.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:4000/";
-//const API_URL = "https://authentication-bee.onrender.com/";
+//const API_URL = "http://localhost:4000/";
+const API_URL = "https://authentication-bee.onrender.com/";
 
 export default API_URL;
